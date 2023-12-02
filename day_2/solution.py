fe8c5e81af883fa75982fa2818028eaa1836c7c0
Gaurav Gosain <itsgauravgosain@gmail.com>
2023-12-02 09:38:46 +0400

    Day 2 🎅

---
+++ day_2/solution.py
@@ -0,0 +1,61 @@
+# Day 2, Lets do this! 🎅
+
+games = open(0).read().strip().split("\n")
+valid_games = []
+part_2 = 0
+
+
+def parse_game(game):
+    data = game.split(": ")
+    game_id = data[0][5:]
+    rounds = data[1].split("; ")
+    rounds = [r.split(", ") for r in rounds]
+
+    return game_id, rounds
+
+
+for game in games:
+    game_id, rounds = parse_game(game)
+
+    valid_games.append(game_id)
+
+    temp_rounds = []
+    color_min = {"blue": 0, "red": 0, "green": 0}
+    found = False
+
+    for idx, subarray in enumerate(rounds):
+        color_counter = {"blue": 0, "red": 0, "green": 0}
+        # Iterate through each element in the subarray
+        for element in subarray:
+            # Split the element into count and color
+            count, color = element.split()
+            # Update the counter for the corresponding color
+            color_counter[color] += int(count)
+
+        if idx == 0:
+            color_min = color_counter
+
+        def get_min(color):
+            if color_min[color] < color_counter[color]:
+                color_min[color] = color_counter[color]
+            return color_min[color]
+
+        color_min["blue"] = get_min("blue")
+        color_min["red"] = get_min("red")
+        color_min["green"] = get_min("green")
+
+        if not found:
+            if (
+                color_counter["blue"] > 14
+                or color_counter["green"] > 13
+                or color_counter["red"] > 12
+            ):
+                valid_games.pop()
+                found = True
+
+    part_2 += color_min["blue"] * color_min["red"] * color_min["green"]
+
+part_1 = sum(map(int, valid_games))
+
+print(part_1)
+print(part_2)
