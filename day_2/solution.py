63bc5dfb3e9ca5b36cedc07e5a5f913b627198ae
Gaurav Gosain <itsgauravgosain@gmail.com>
2023-12-02 10:56:38 +0400

    More cleanup

--- day_2/solution.py
+++ day_2/solution.py
@@ -1,9 +1,11 @@
+import math  # 💻
+
 # Day 2, Lets do this! 🎅
 
 games = open(0).read().strip().split("\n")
 valid_games = []
 part_2 = 0
-default_color_counter = {"blue": 0, "red": 0, "green": 0}
+reset_color_counter = lambda: {"blue": 0, "green": 0, "red": 0}
 
 
 def parse_game(game):
@@ -11,23 +13,21 @@ def parse_game(game):
 
     game_id = game_id[5:]
 
-    rounds = rounds.split("; ")
-    rounds = [r.split(", ") for r in rounds]
+    rounds = [r.split(", ") for r in rounds.split("; ")]
 
     return game_id, rounds
 
 
 for game in games:
     game_id, rounds = parse_game(game)
-
     valid_games.append(game_id)
 
     temp_rounds = []
-    color_min = default_color_counter
+    color_min = reset_color_counter()
     found = False
 
     for idx, subarray in enumerate(rounds):
-        color_counter = default_color_counter
+        color_counter = reset_color_counter()
         # Iterate through each element in the subarray
         for element in subarray:
             # Split the element into count and color
@@ -38,14 +38,10 @@ for game in games:
         if idx == 0:
             color_min = color_counter
 
-        def get_min(color):
-            if color_min[color] < color_counter[color]:
-                color_min[color] = color_counter[color]
-            return color_min[color]
-
-        color_min["blue"] = get_min("blue")
-        color_min["red"] = get_min("red")
-        color_min["green"] = get_min("green")
+        color_min = {
+            color: max(color_min[color], color_counter[color])
+            for color in color_min.keys()
+        }
 
         if not found:
             if (
@@ -56,7 +52,7 @@ for game in games:
                 valid_games.pop()
                 found = True
 
-    part_2 += color_min["blue"] * color_min["red"] * color_min["green"]
+    part_2 += math.prod(color_min.values())
 
 part_1 = sum(map(int, valid_games))
 
